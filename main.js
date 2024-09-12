2453e3ee6c1e05c8d695efee6053d30407871cc7
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-12 23:16:46 +0300

    q3

--- main.js
+++ main.js
@@ -10,10 +10,25 @@ class Person {
     }
 }
 
+class Student extends Person{
+    constructor(name, age, career, studentId){
+        super(name, age, career);
+        this.studentId = studentId;
+    }
+
+    greet(){
+        console.log(`Hi my name is ${this.name}, I'm ${this.age} years old, works ${this.career} and my studentID is ${this.studentId}!`);
+        
+    }
+}
+
 const person1 = new Person ('Ivan', '21', 'admin');
 const person2 = new Person ('Valentyna', '21', 'finansist');
 
 person1.greet();
 person2.greet();
 
+const student1 = new Student('Ivan', '21', 'admin', 'TE13160019');
+
+student1.greet();
 
