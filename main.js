88546383771d087592847e6b9a61c40621d37bc8
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-13 00:03:55 +0300

    q5

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
-class Shape {
-  calculateArea() {
-    console.log("Shape does not indificate!");
-    return 0;
-  }
+class Book{
+    constructor(title, year, author){
+        this.title = title;
+        this.year = year;
+        this.author = author
+    }
 }
 
-class Circle extends Shape{
-    constructor(radius){
-        super();
-        this.radius = radius;
+class Library{
+    constructor(){
+        this.books = [];
     }
 
+    addBook(book){
+        this.books.push(book);
+        console.log(`The book ${book.title} add to library!`);
+    }
 
-calculateArea() {
-    const area = Math.PI * Math.pow(this.radius, 2);
-    console.log(`the area of ​​a circle with ${this.radius} radius equal to ${area.toFixed(2)}`);
-    return area;
+    listBooks() {
+        if(this.books.length === 0) {
+            console.log("Library is empty!");
+            return;
+        }
+    
+        console.log("List of books in this library:");
+        this.books.forEach((book, index) => {
+            console.log(`${index + 1} Title: ${book.title}, Author: ${book.author}, Year of druk: ${book.year}`);
+        });
+    }
 }
-}
 
-const circle1 = new Circle(18);
+const library = new Library ();
 
-circle1.calculateArea();
+const book1 = new Book ('Harry Potter and the Philosofers stone', '1997', 'J. K. Rowling',);
+const book2 = new Book ('Harry Potter and the Philosofers stone', '1997', 'J. K. Rowling',);
+const book3 = new Book ('Harry Potter and the Philosofers stone', '1997', 'J. K. Rowling',);
+library.addBook(book1);
+library.addBook(book2);
+library.addBook(book3);
+
+library.listBooks();
+
