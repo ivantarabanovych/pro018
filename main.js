6ea0439d4393f0528e5b30407df86e3d6b251441
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-12 23:04:18 +0300

    q2

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const OOPConclusion = "Об'єктно-орієнтоване програмування (ООП) надає переваги в управлінні складними системами завдяки модульності, інкапсуляції, спадкуванню та поліморфізму. Воно підходить для великих проектів, де важлива структурована архітектура та повторне використання коду. Однак, цей підхід може бути складнішим для новачків і менш ефективним за ресурсами в порівнянні з процедурним підходом.";
-const proceduralConclusion = "Процедурне програмування є простішим і більш ефективним для невеликих програм, орієнтованих на виконання послідовних інструкцій або процедур. Воно забезпечує прозорість і швидкість розробки для невеликих проектів. Проте, цей підхід має свої обмеження в масштабованості, повторюваності коду та управлінні складними системами, оскільки не має інструментів для інкапсуляції та управління залежностями, як у ООП.";
+class Person {
+    constructor(name, age, career){
+        this.name = name;
+        this.age = age;
+        this.career = career; 
+    }
 
-document.getElementById('OOPConclusion').innerText = OOPConclusion;
-document.getElementById('proceduralConclusion').innerText = proceduralConclusion;
+    greet(){
+        console.log(`Hi my name is ${this.name}, I'm ${this.age} years old, works ${this.career}!`);
+    }
+}
 
-document.getElementById('OOPConclusion').style.color = "green";
-document.getElementById('proceduralConclusion').style.color = "red";
+const person1 = new Person ('Ivan', '21', 'admin');
+const person2 = new Person ('Valentyna', '21', 'finansist');
 
-document.getElementById('OOPConclusion').style.maxWidth = "500px";
-
-document.getElementById('proceduralConclusion').style.maxWidth = "500px";
-
-document.getElementById('block').style.display = "flex";
-document.getElementById('block').style.justifyContent = "space-beetwen";
-document.getElementById('block').style.gap = "90px";
-
+person1.greet();
+person2.greet();
 
 
