7d856a5a43bdffc5c7b91edcfb7f95d7774027d7
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-15 13:05:52 +0300

    q6

--- main.js
+++ main.js
@@ -1,42 +1,49 @@
-class Book{
-    constructor(title, year, author){
-        this.title = title;
-        this.year = year;
-        this.author = author
+class Component{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
     }
 }
 
-class Library{
-    constructor(){
-        this.books = [];
+class Order {
+    constructor() {
+        this.components = [];
     }
 
-    addBook(book){
-        this.books.push(book);
-        console.log(`The book ${book.title} add to library!`);
+    addComponent(component, quantity){
+        this.components.push({component, quantity});
+        console.log(`Додано ${quantity} деталі(ей) ' ${component.name} ' до замовлення`);
+    }
+    
+    calculateTotal(){
+        let total = 0;
+        this.components.forEach(item =>{
+            total += item.component.price * item.quantity;
+        });
+        return total;
     }
 
-    listBooks() {
-        if(this.books.length === 0) {
-            console.log("Library is empty!");
+    printOrderDetails(){
+        if(this.components.length === 0){
+            console.log("Замовлення порожнє");
             return;
         }
-    
-        console.log("List of books in this library:");
-        this.books.forEach((book, index) => {
-            console.log(`${index + 1} Title: ${book.title}, Author: ${book.author}, Year of druk: ${book.year}`);
+
+        console.log("Деталі замовлення:");
+        this.components.forEach(item => {
+            console.log(`${item.quantity} деталі(ей) "${item.component.name}" - $${(item.component.price * item.quantity).toFixed(2)}`);
         });
+
+        console.log(`Загальна сума замовлення: $${this.calculateTotal().toFixed(2)}`);
     }
 }
 
-const library = new Library ();
+const timingBelt = new Component ('timingBelt', 50);
+const olinolLubricant = new Component ('olinolLubricant', 20);
 
-const book1 = new Book ('Harry Potter and the Philosofers stone', '1997', 'J. K. Rowling',);
-const book2 = new Book ('Harry Potter and the Philosofers stone', '1997', 'J. K. Rowling',);
-const book3 = new Book ('Harry Potter and the Philosofers stone', '1997', 'J. K. Rowling',);
-library.addBook(book1);
-library.addBook(book2);
-library.addBook(book3);
+const order = new Order();
 
-library.listBooks();
+order.addComponent(timingBelt, 2);
+order.addComponent(olinolLubricant, 3);
 
+order.printOrderDetails();
